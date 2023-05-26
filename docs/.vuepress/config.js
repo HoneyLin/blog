e0d8dfff47d7346f98302cad91cbbbb801e8154a
howie.lin <howie.lin@presslogic.com>
2023-05-26 15:59:41 +0800

    init deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,16 @@
-import { defineUserConfig } from 'vuepress'
+import {defineUserConfig, defaultTheme} from 'vuepress'
 
 export default defineUserConfig({
     lang: 'zh-CN',
     title: '你好， VuePress ！',
     description: '这是我的第一个 VuePress 站点',
     base: '/blog/',
+    theme: defaultTheme({
+        navbar: [
+            {
+                text: '首页',
+                link: '/'
+            }
+        ]
+    })
 })
